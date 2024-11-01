f98df07176f15853beab0165b28cf3cc003bfee9
Martin Prokopič <martin@prokopic.net>
2024-11-01 22:26:42 +0100

    Remove newlines from QR, but keep them in table

--- src/utils/getReceiverMessage.ts
+++ src/utils/getReceiverMessage.ts
@@ -1,13 +1,13 @@
 import { FormSchema } from "../App";
 
-export const getReceiverMessage = (data: FormSchema) => {
+export const getReceiverMessage = (data: FormSchema, compact: boolean = false) => {
 	const wanted = [
 		data.wantGiftConfirmation && "potvrzení o daru",
 		data.wantBeOnWeb && "být na webu",
 	].filter(Boolean);
 
     return [
-        data.members.map((m) => `${m.name} (${m.birth}${m.isLeader ? ", činovník" : m.isSponsor ? ", sponzor" : ""})`).join("\n"),
-        wanted.length ? `Chci ${wanted.join(" a ")}.` : "",
-    ].join("\n");
+        data.members.map((m) => `${m.name} (${m.birth}${m.isLeader ? ", činovník" : m.isSponsor ? ", sponzor" : ""})`).join(compact ? ", " : "\n"),
+        wanted.length && `Chci ${wanted.join(" a ")}.`,
+    ].filter(Boolean).join(compact ? ". " : "\n");
 };
